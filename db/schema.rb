ea008233be72fde6b7413a954e175e36ce1edd6c
Kota Kanazawa <kota0818@gmail.com>
2020-08-20 14:02:35 +0900

    root画面をブログindexに

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_002808) do
+ActiveRecord::Schema.define(version: 2020_08_20_045959) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
